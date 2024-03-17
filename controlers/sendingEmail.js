fa29de4182716ba1c206a569e6d11e4f72f060a0
Ceres Rasmussen <112823848+CeresRasmussen@users.noreply.github.com>
2024-03-17 22:48:19 +0200

    some fix

--- controlers/sendingEmail.js
+++ controlers/sendingEmail.js
@@ -2,16 +2,26 @@ const { controllerWrapper, sendEmail } = require("../helpers/index");
 
 const sendingEmail = async (req, res, next) => {
   const { name, comment } = req.body;
-
-  const emailText = {
-    to: "sobkowowa@gmail.com",
-    subject: "Пропозиції з сайту",
-    text: `Користувач ${name} залишив наступний коментар "${comment}"`,
+  let emailText = {
+    to: "com@oe.cv.ua",
+    // to: "sobkowowa@gmail.com",
+    subject: "Пропозиції для АТ «Чернівціобленерго»",
+    text: "",
   };
 
-  await sendEmail(emailText);
+  if (name) {
+    emailText.text = `Користувач ${name} залишив наступний коментар: "${comment}"`;
+  } else {
+    emailText.text = `Анонімний залишив наступний коментар: "${comment}"`;
+  }
 
-  res.status(201).json("Повідомлення відіслано");
+  try {
+    await sendEmail(emailText);
+    res.status(201).json("Повідомлення відіслано");
+  } catch (error) {
+    console.error("Error sending email:", error);
+    res.status(500).json("Помилка відправлення повідомлення");
+  }
 };
 
 module.exports = {
